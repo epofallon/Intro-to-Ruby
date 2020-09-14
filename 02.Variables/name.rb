b7ea892c33e29a6dd5a8d61a058d6831818121dd
Eugene O'Fallon <epofallon@gmail.com>
2020-09-14 19:09:28 +0000

    Add exercises for chapter 2: Variables

---
+++ 02.Variables/name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Your full name is:"
+puts "#{first_name} #{last_name}"
+#puts "Hello #{name}! Welcome to Intro to Learning Ruby!"
+
+#10.times {puts name}
---
+++ 02.Variables/scope.rb
@@ -0,0 +1,16 @@
+a = 5
+
+3.times {|n|
+  a = 3
+  b = 5
+}
+
+puts a
+
+arr = [1,2,3]
+
+for i in arr do
+  a = 5
+end
+
+puts a
